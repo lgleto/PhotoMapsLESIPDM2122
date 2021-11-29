e465b268467f3916be4e858f340a662255e73284
Lourenço Gomes <leto.lg@gmail.com>
2021-11-29 20:47:38 +0000

    first commit

---
+++ app/src/main/java/ipca/example/photomap/LoginActivity.kt
@@ -0,0 +1,34 @@
+package ipca.example.photomap
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        auth.createUserWithEmailAndPassword("leto.lg@gmail.com", "12345678")
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                }
+            }
+
+    }
+}
